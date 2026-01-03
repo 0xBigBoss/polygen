9de3c80bfc59068704d59cf15035591fbb6aa262
Big Boss <bigboss@metalrodeo.xyz>
2026-01-03 07:32:14 -0600

    fix(ios): Add bridgeless mode TurboModule registration support
    In React Native 0.81+ bridgeless mode, the +load registration via
    registerCxxModuleToGlobalModuleMap doesn't work because bridgeless
    mode uses RCTTurboModuleManagerDelegate instead of the global map.
    
    This adds a getTurboModule:jsInvoker: method that app developers
    can call from their AppDelegate to register the module manually:
    
      - (std::shared_ptr<TurboModule>)getTurboModule:(const std::string &)name
                                            jsInvoker:(std::shared_ptr<CallInvoker>)jsInvoker
      {
        if (auto module = [Wasm getTurboModule:name jsInvoker:jsInvoker]) {
          return module;
        }
        return [super getTurboModule:name jsInvoker:jsInvoker];
      }
    
    The +load registration is preserved for backward compatibility with
    bridged mode.
    
    Fixes: https://github.com/0xBigBoss/polygen/issues/1

--- packages/polygen/ReactNativePolygen.podspec
+++ packages/polygen/ReactNativePolygen.podspec
@@ -15,7 +15,7 @@ Pod::Spec.new do |s|
   s.source       = { :git => "https://github.com/callstack/polygen.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm}", "cpp/ReactNativePolygen/**/*.{hpp,cpp,c,h}"
-  s.public_header_files = "ios/ReactNativePolygen/**/*.h", "cpp/ReactNativePolygen/**/*.h"
+  s.public_header_files = "ios/**/*.h", "cpp/ReactNativePolygen/**/*.h"
   s.header_mappings_dir = "cpp/ReactNativePolygen"
 
   # Use install_modules_dependencies helper to install the dependencies if React Native version >=0.71.0.
--- packages/polygen/ios/Wasm.mm
+++ packages/polygen/ios/Wasm.mm
@@ -3,12 +3,27 @@
 
 @implementation Wasm
 
+/**
+ * Registers the module to the global module map during class load.
+ * This works for bridged mode but may not work in bridgeless mode (RN 0.81+).
+ * For bridgeless mode, use getTurboModule:jsInvoker: from AppDelegate.
+ */
 + (void)load {
   facebook::react::registerCxxModuleToGlobalModuleMap(
-  std::string(facebook::react::ReactNativePolygen::kModuleName),
-  [&](std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
-  return std::make_shared<facebook::react::ReactNativePolygen>(jsInvoker);
-});
+    std::string(facebook::react::ReactNativePolygen::kModuleName),
+    [](std::shared_ptr<facebook::react::CallInvoker> jsInvoker) {
+      return std::make_shared<facebook::react::ReactNativePolygen>(jsInvoker);
+    }
+  );
+}
+
++ (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
+                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
+{
+  if (name == facebook::react::ReactNativePolygen::kModuleName) {
+    return std::make_shared<facebook::react::ReactNativePolygen>(jsInvoker);
+  }
+  return nullptr;
 }
 
 @end
